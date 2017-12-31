578a8991b1520e49ffe94863a19728f2f5f765db
Nivethaselvakumar <34640693+Nivethaselvakumar@users.noreply.github.com>
2017-12-31 18:09:31 +0530

    Create stringtoupperprog7.java

---
+++ assignment/first/stringtoupperprog7.java
@@ -0,0 +1,20 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	String strUpper;
+        Scanner scan = new Scanner(System.in);
+     
+        strUpper = scan.nextLine();
+        
+        
+        System.out.print(strUpper.toUpperCase());
+	}
+}
