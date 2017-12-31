1dd05ddbbcc2956923d67cca0e0a2100dcf76872
Nivethaselvakumar <34640693+Nivethaselvakumar@users.noreply.github.com>
2017-12-31 16:04:53 +0530

    Create factorialwithrecprog3.c

---
+++ assignment/first/factorialwithrecprog3.c
@@ -0,0 +1,25 @@
+
+
+import java.util.*;
+
+
+class Factorial{  
+ static int factorial(int n){    
+  if (n == 0)    
+    return 1;    
+  else    
+    return(n * factorial(n-1));    
+ }    
+ public static void main(String args[]){  
+  int i,fact=1;  
+  int number;
+   Scanner sc=new Scanner(System.in);
+ 
+   
+	  
+       number=sc.nextInt();
+ 
+  fact = factorial(number);   
+  System.out.println("Factorial is"+fact);    
+ }  
+}  
